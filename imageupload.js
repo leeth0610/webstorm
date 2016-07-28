35e361ced1b33fc82a8739e1f937427eca1bddbe
leeth0610 <leeth0610@gmail.com>
2016-07-29 01:45:06 +0900

    lee

--- imageupload.js
+++ imageupload.js
@@ -2,6 +2,7 @@ const formidable = require('formidable');
 const randomString = require('randomstring');
 const pathutil = require('path');
 const AWS = require('aws-sdk');
+const fs = require('fs');
 
 AWS.config.region = 'ap-northeast-2';
 AWS.config.accessKeyId = 'AKIAJQUEKAV4BVTB5A3Q';
@@ -10,11 +11,9 @@ AWS.config.secretAccessKey = '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';
 const s3 = new AWS.S3();
 
 const instance = function (req, res) {
-        try {
-                getFile(req).then(toS3, console.err).then(res.send, console.err);
-        } catch (err) {
-                console.log(err);
-        }
+        getFile(req).then(toS3, console.err).then((result) => {
+                res.send(result);
+        }, console.err);
 };
 
 const uploadDir = __dirname + '/upload';
@@ -39,6 +38,7 @@ function getFile(req) {
                         console.log('3');
                         resolve(
                                 {
+                                        title: fields.title,
                                         image: file
                                 }
                         );
@@ -57,7 +57,8 @@ function toS3(result) {
 
                 const readStream = fs.createReadStream(path);
                 const key = 'public/' + newFileName + extname;
-                const param = {
+                console.log(key);
+                const params = {
                         Bucket: 'leeth0610',
                         Key: key,
                         ACL: 'public_read',
@@ -65,15 +66,16 @@ function toS3(result) {
                         ContentType: contentType
                 };
 
-                s3.putObject(param, (err, data) => {
+                s3.putObject(params, (err, data) => {
                         console.log('6');
-                        if (err) {
-                                console.log(err);
+                        if (err.toString()) {
+                                console.log('err: ', err);
                                 reject(err);
                                 return;
                         }
 
-                        const imageUrl = s3.getSignedUrl('getObject', {Bucket: param.Bucket, Key: param.key});
+                        const imageUrl = s3.getSignedUrl('getObject', {Bucket: params.Bucket, Key: params.Key});
+
 
                         console.log('7');
                         resolve({
